79cf6de1fcdeb3679e041bfb62ed2213b35cff26
Alexandr <sasha-bragin@mail.ru>
2023-02-27 17:01:02 +0300

    Семина2

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+// Например:
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+
+// Console.WriteLine();
+// Console.ReadLine();
+Random rand = new Random();
+// rand.Next(10, 100);
+int number = rand.Next(10, 100);
+Console.WriteLine("Случайное число " + number);
+int max = number /10;
+int min = number % 10;
+if ( max > min)
+Console.WriteLine("Наибольшая цифра числа это {0}", max);
+else Console.WriteLine("Наибольшая цифра числа это {0}", min);
---
+++ Task3/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли первое число кратным второму. Если число 1 не кратно числу 2, то программа выводит остаток от деления.
+
+// 34, 5 -> не кратно, остаток 4 
+// 16, 4 -> кратно
+
+// Задача №14.
+// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 
+// 7 и 23.
+
+// 14 -> нет 
+// 46 -> нет 
+// 161 -> да
+
+
+
+// Console.ReadLine("Введите первое число");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.ReadLine("Введите 2 число");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// if ( number1 % number2 == 0)
+// Console.WriteLine( number1 + " кратно " + number2);
+// else {
+
+// Console.WriteLine ( number1 " некратно " + number2);
+// Console.WriteLine " Остаток от деления: " + number1 % number2 );
+// }
+
+
+Console.ReadLine(" Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if ( number % 7 == 0 && number % 23 == 0 )
+Console.WriteLine(" кратно 7 и 23 ");
+else 
+Console.WriteLine (" не кратно 7 и 23 ");
+
+
+
