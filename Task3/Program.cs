3159074982de966420c4ce5990a9269e03d33996
Alexandr <sasha-bragin@mail.ru>
2023-03-02 14:35:59 +0300

    добавил решение в таск 3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -6,10 +6,10 @@
 // 34, 5 -> не кратно, остаток 4 
 // 16, 4 -> кратно
 
-// Console.WriteLine("Введите первое число");
-// int numberA = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите 2 число");
-// int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число");
+int numberB = Convert.ToInt32(Console.ReadLine());
 
 // if ( numberA % numberB == 0)
 // Console.WriteLine( numberA + " кратно " + numberB);
@@ -19,9 +19,9 @@
 // Console.WriteLine ("Остаток от деления: " + numberA % numberB );
 // }
 
-// string result = numberA%numberB == 0? "Первое число кратно второму":$"Первое некратно второму. Остаток от деления = {numberA%numberB}";
+string result = numberA % numberB == 0 ? "Первое число кратно второму" : $"Первое некратно второму. Остаток от деления = {numberA % numberB}";
 
-
+Console.WriteLine(result);
 
 
 
